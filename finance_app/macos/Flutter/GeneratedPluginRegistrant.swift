5415ee20aaf8a541a5db0c6e61f5476264d5f9d2
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2024-09-07 17:50:42 +0200

    Revert "gradle changes"
    This reverts commit 058a11b36bf85975efb0bb45721f8ee9c5a12657.

--- finance_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ finance_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus
-import path_provider_foundation
+import path_provider_macos
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
